c63839009a52d4d8fd8338eae50dada2f33d4350
Julio Cesar <juliosantosjob>
2024-06-13 17:59:43 -0300

    Add scenario: Errors in registration with all fields blank

---
+++ README.md
@@ -0,0 +1,40 @@
+# Playwright-E2E-Tests-Petlov
+
+## Overview
+This project aims to test the [PetLov](https://petlov.vercel.app/) website, ensuring that the registration process works correctly.
+
+## Prerequisites
+Ensure you have the following installed:
+- Node.js 
+- npm 
+
+## Getting Started
+1. Clone this repository.
+    ```bash
+    git clone https://github.com/juliosantosjob/Playwright-E2E-Tests-Petlov.git
+    ```
+
+2. Navigate to the project directory.
+    ```bash
+    cd project-directory
+    ```
+
+3. Install dependencies.
+    ```bash
+    npm install
+    ```
+
+4. Run the project.
+    ```bash
+    npm run test
+    ```
+    
+## Networks
+###
+<div>
+
+[<img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" />](https://www.linkedin.com/in/julio-santos-43428019b)
+[<img src = "https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white">](https://www.instagram.com/juli0sts/)
+[<img src = "https://img.shields.io/badge/facebook-%231877F2.svg?&style=for-the-badge&logo=facebook&logoColor=white">](https://www.facebook.com/profile.php?id=100003793058455)
+<a href="mailto:julio958214@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank">
+</a> 
--- src/tests/regist.spec.ts
+++ src/tests/regist.spec.ts
@@ -4,7 +4,7 @@ import { RegisterPage } from '../pages/regist.page';
 import { createDonationPoint } from '../samples/regist.sample';
 import { DonationPoint } from '../types/donationPoint.type';
 
-test.describe('Register a donation point', () => {
+test.describe('Donation point registration', () => {
   let homePage: HomePage;
   let registerPage: RegisterPage;
   let donationPoint: DonationPoint;
@@ -18,13 +18,13 @@ test.describe('Register a donation point', () => {
     await registerPage.goToRegister();
   });
 
-  test('Register a valid donation point', async () => {
+  test('Must successfully register a donation point', async () => {
     await registerPage.fillForm(donationPoint);
     await registerPage.submitForm();
     await registerPage.verifySuccess('Você fez a diferença!');
   });
 
-  test('Do not register with invalid email', async () => {
+  test('Should not register with an invalid email', async () => {
     donationPoint.email = 'invalid_email.com';
 
     await registerPage.fillForm(donationPoint);
@@ -32,14 +32,14 @@ test.describe('Register a donation point', () => {
     await registerPage.verifyError('Informe um email válido');
   });
 
-  test('Do not register with invalid CEP', async () => {
+  test('Should not register with an invalid cep', async () => {
     donationPoint.cep = '00000';
 
     await registerPage.fillForm(donationPoint);
     await registerPage.verifyError('Informe um CEP válido');
   });
 
-  test('Do not register with invalid address number', async () => {
+  test('Should not register with an invalid address number', async () => {
     donationPoint.addressNumber = '00000';
 
     await registerPage.fillForm(donationPoint);
@@ -47,7 +47,7 @@ test.describe('Register a donation point', () => {
     await registerPage.verifyError('Informe um número maior que zero');
   });
 
-  test('Registration with all fields blank', async () => {
+  test('Errors in registration with all fields blank', async () => {
     const errorMessages = [
       'Informe o seu nome completo',
       'Informe o seu melhor email',
